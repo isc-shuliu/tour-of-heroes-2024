ac42d08a5c6027e2147de66e855bb10419a8d5ec
isc-tleavitt <73311181+isc-tleavitt@users.noreply.github.com>
2023-06-12 16:58:41 -0400

    Part 2

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,11 +2,23 @@ import { browser, element, by, ElementFinder } from 'protractor';
 
 const expectedH1 = 'Tour of Heroes';
 const expectedTitle = `${expectedH1}`;
+const expectedH2 = 'My Heroes';
+const targetHero = { id: 16, name: 'RubberMan' };
+const nameSuffix = 'X';
 
 class Hero {
   constructor(public id: number, public name: string) {}
 
-  // Factory method
+  // Factory methods
+
+  // Get hero from s formatted as '<id> <name>'.
+  static fromString(s: string): Hero {
+    return new Hero(
+      +s.substring(0, s.indexOf(' ')),
+      s.slice(s.indexOf(' ') + 1),
+    );
+  }
+
   // Get hero id and name from the given detail element.
   static async fromDetail(detail: ElementFinder): Promise<Hero> {
     // Get hero id from the first <div>
@@ -20,47 +32,98 @@ class Hero {
   }
 }
 
-const nameSuffix = 'X';
-async function addToHeroName(text: string): Promise<void> {
-  const input = element(by.css('input'));
-  await input.sendKeys(text);
-}
-
-describe('Tutorial part 1', () => {
-
-  const expectedHero = { id: 1, name: 'Windstorm' };
-
+describe('Tutorial part 2', () => {
   beforeAll(() => browser.get(''));
+  describe('Initial page', initialPageTests);
+  describe('Select hero', selectHeroTests);
+  describe('Update hero', updateHeroTests);
+});
 
+function initialPageTests() {
   it(`has title '${expectedTitle}'`, async () => {
     expect(await browser.getTitle()).toEqual(expectedTitle);
   });
 
   it(`has h1 '${expectedH1}'`, async () => {
-    const hText = await element(by.css('h1')).getText();
-    expect(hText).toEqual(expectedH1, 'h1');
+    await expectHeading(1, expectedH1);
   });
 
-  it(`shows initial hero details`, async () => {
+  it(`has h2 '${expectedH2}'`, async () => {
+    await expectHeading(2, expectedH2);
+  });
+
+  it('has the right number of heroes', async () => {
+    const page = getPageElts();
+    expect(await page.heroes.count()).toEqual(9);
+  });
+
+  it('has no selected hero and no hero details', async () => {
+    const page = getPageElts();
+    expect(await page.selected.isPresent()).toBeFalsy('selected hero');
+    expect(await page.heroDetail.isPresent()).toBeFalsy('no hero detail');
+  });
+}
+
+function selectHeroTests() {
+  it(`selects ${targetHero.name} from hero list`, async () => {
+    const hero = element(by.cssContainingText('button span.badge', targetHero.id.toString()));
+    await hero.click();
+    // Nothing specific to expect other than lack of exceptions.
+  });
+
+  it(`has selected ${targetHero.name}`, async () => {
+    const page = getPageElts();
+    const expectedText = `${targetHero.id} ${targetHero.name}`;
+    expect((await page.selected.getText()).replace('\n', ' ')).toBe(expectedText);
+  });
+
+  it('shows selected hero details', async () => {
     const page = getPageElts();
     const hero = await Hero.fromDetail(page.heroDetail);
-    expect(hero.id).toEqual(expectedHero.id);
-    expect(hero.name).toEqual(expectedHero.name.toUpperCase());
+    expect(hero.id).toEqual(targetHero.id);
+    expect(hero.name).toEqual(targetHero.name.toUpperCase());
   });
+}
 
-  it(`shows updated hero name`, async () => {
+function updateHeroTests() {
+  it(`can update hero name`, async () => {
     await addToHeroName(nameSuffix);
+    // Nothing specific to expect other than lack of exceptions.
+  });
+
+  it(`shows updated hero name in details`, async () => {
     const page = getPageElts();
     const hero = await Hero.fromDetail(page.heroDetail);
-    const newName = expectedHero.name + nameSuffix;
-    expect(hero.id).toEqual(expectedHero.id);
+    const newName = targetHero.name + nameSuffix;
+    expect(hero.id).toEqual(targetHero.id);
     expect(hero.name).toEqual(newName.toUpperCase());
   });
 
-});
+  it(`shows updated hero name in list`, async () => {
+    const page = getPageElts();
+    const hero = Hero.fromString((await page.selected.getText()).replace('\n', ' '));
+    const newName = targetHero.name + nameSuffix;
+    expect(hero.id).toEqual(targetHero.id);
+    expect(hero.name).toEqual(newName);
+  });
+
+}
+
+async function addToHeroName(text: string): Promise<void> {
+  const input = element(by.css('input'));
+  await input.sendKeys(text);
+}
+
+async function expectHeading(hLevel: number, expectedText: string): Promise<void> {
+  const hTag = `h${hLevel}`;
+  const hText = await element(by.css(hTag)).getText();
+  expect(hText).toEqual(expectedText, hTag);
+}
 
 function getPageElts() {
   return {
-    heroDetail: element(by.css('app-root'))
+    heroes: element.all(by.css('app-root li button')),
+    selected: element(by.css('app-root li button.selected')),
+    heroDetail: element(by.css('app-root > div, app-root > app-heroes > div'))
   };
 }
